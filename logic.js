0bf6199f393dc2f528cff8e275428f3b46018da4
tip4iik-spec <tip4iik@gmail.com>
2026-02-20 21:02:55 +0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,17 +1,20 @@
-container = document.getElementById("container-for-templates");
-temp = document.getElementById("test-template");
+console.log("Контейнер:", document.getElementById("templates-container"));
+console.log("Шаблон:", document.getElementById("item-template"));
+
+container = document.getElementById("templates-container");
+temp = document.getElementById("item-template");
 
 temp_clone = temp.content.cloneNode(true);
 
-img_el = temp_clone.querySelector(".image-div");
+img_el = temp_clone.querySelector(".item-image");
 temp_image = document.createElement('img');
 temp_image.src = "./Crimson_Planet.png";
 img_el.appendChild(temp_image);
 
-name_el = temp_clone.querySelector(".name");
+name_el = temp_clone.querySelector(".item-title");
 name_el.textContent = "Crimson Planet";
 
-desk_el = temp_clone.querySelector(".desk");
+desk_el = temp_clone.querySelector(".item-description");
 desk_el.textContent = "A planet with a crimson hue, known for its unique atmosphere and mysterious landscapes.";
 
 container.appendChild(temp_clone);
